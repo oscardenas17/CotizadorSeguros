17a01c32eb40780c20b4415ac4a33045fff3b96a
Oscar Yesid Cardenas Santos <oscardenas17@gmail.com>
2020-04-16 23:30:58 -0500

    Leyendo y validando datos ingresados en formulario

--- cotizaSeguro/js/app.js
+++ cotizaSeguro/js/app.js
@@ -1,8 +1,58 @@
 //COTIZADOR CONSTRUCTOR
 
-function Seguro(){
+function Seguro(marca, anio, tipo){
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
+
+//Todo lo que se muestra en interfaz
+function Interfaz(){
 
 }
+
+//Event listener
+const formulario = document.getElementById('cotizar-seguro');
+
+formulario.addEventListener('submit', function(e){
+    
+    e.preventDefault();
+
+    //Leer la marca seleccionada del select
+    const marca = document.getElementById('marca');
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+    //Leer el año seleccionado del select
+    const anio = document.getElementById('anio');
+    const anioSeleccionado = anio.options[anio.selectedIndex].value;
+    
+
+    //Lee el valor del radio button
+    const tipo= document.querySelector('input[name = "tipo"]:checked').value;
+
+    //crear instancia de interfaz
+    const interfaz = new Interfaz();
+    //Revisamos que los campos no estenn vacios
+    if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
+        //Interfaz imprimiendo un error
+        //console.log('Faltan datos');
+
+    }else{
+        //Instanciar seguro y mostrar interfaz
+        //console.log('todo correcto')
+        
+    }
+
+
+
+    //console.log(anioSeleccionado);
+    //console.log(marcaSeleccionada);
+    //console.log('presionado');
+});
+
+    
+   
+
 
 
 const max = new Date().getFullYear(),
